6bdc5c1477fc2929db7225cdd4e7be6c016423c3
Rodrigo Dias <rodiasbass@gmail.com>
2017-07-30 15:20:06 -0300

    [FT] Add buybutton component

---
+++ src/components/Buybutton.vue
@@ -0,0 +1,17 @@
+<template>
+	<button @click="launchAction()" class="btn buy-btn clear" :type="tipo">{{ rotulo }}</button>
+</template>
+
+<script>
+	export default {
+		props: ['tipo','rotulo'],
+
+		methods: {
+			launchAction() {
+				//if(confirm('Deseja Adicionar esse produto ao carrinho?')) {
+					this.$emit('btn-activated');
+				//}				
+			}
+		}
+	}
+</script>
